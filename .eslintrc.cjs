748b1f7e66d22ba1a235ac24e3cffdf1ee18aea1
Travis Hoki <travis.hoki@gmail.com>
2026-05-18 09:47:21 -0600

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2020: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
+    ],
+    ignorePatterns: ["dist"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react-refresh"],
+    rules: {
+        "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    },
+}
--- src/CategoryMenu/CategoryMenu.tsx
+++ src/CategoryMenu/CategoryMenu.tsx
@@ -1,6 +1,3 @@
-import { useMemo } from "react"
-import { useParams } from "react-router-dom"
-import { subCategoriesForTop } from "./CategoryMenu.helpers"
 import { Menu } from "./Menu/Menu"
 import { SubMenu } from "./Subcategory/SubMenu"
 import "./CategoryMenu.css"
