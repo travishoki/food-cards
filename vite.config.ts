f47f0567665ffd37d2bd9183c748e08d5cdf32e4
Travis Hoki <travis.hoki@gmail.com>
2026-05-18 15:24:48 -0600

    use scss variables

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@use "/src/styles/variables" as *;`,
+			},
+		},
+	},
 	plugins: [react()],
 	server: {
 		host: true,
