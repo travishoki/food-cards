511a7b06cee6926fd6eec6f5b8478cf5fd96bd4b
Travis Hoki <travis.hoki@gmail.com>
2026-05-19 17:13:04 -0600

    breakout CategoryClearLink

---
+++ src/FoodsPage/NoResults/CategoryClearLink.tsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+
+import { CATEGORY_DATA, TOP_CATEGORY_DATA } from "../../data/categories";
+
+type CategoryClearLinkProps = {
+	subCategory?: string;
+	topCategory?: string;
+};
+
+export const CategoryClearLink = ({
+	subCategory,
+	topCategory,
+}: CategoryClearLinkProps) => {
+	const topLabel =
+		topCategory && topCategory in TOP_CATEGORY_DATA
+			? TOP_CATEGORY_DATA[topCategory as keyof typeof TOP_CATEGORY_DATA]
+					.label
+			: undefined;
+	const subLabel =
+		subCategory && CATEGORY_DATA[subCategory]
+			? CATEGORY_DATA[subCategory].label
+			: undefined;
+
+	if (subCategory && topCategory) {
+		return (
+			<Link className="no-results__button" to={`/${topCategory}`}>
+				Show All &ldquo;{topLabel ?? topCategory}&rdquo;
+			</Link>
+		);
+	}
+
+	if (!subCategory && topCategory) {
+		return (
+			<Link className="no-results__button" to="/">
+				Clear Category
+			</Link>
+		);
+	}
+
+	if (subCategory && !topCategory && subLabel) {
+		return (
+			<Link className="no-results__button" to="/">
+				Clear Category
+			</Link>
+		);
+	}
+
+	return null;
+};
--- src/FoodsPage/NoResults/NoResults.tsx
+++ src/FoodsPage/NoResults/NoResults.tsx
@@ -1,6 +1,4 @@
-import { Link } from "react-router-dom";
-
-import { CATEGORY_DATA, TOP_CATEGORY_DATA } from "../../data/categories";
+import { CategoryClearLink } from "./CategoryClearLink";
 
 import "./NoResults.scss";
 
@@ -18,57 +16,32 @@ export const NoResults = ({
 	onClearSearch,
 	subCategory,
 	topCategory,
-}: NoResultsProps) => {
-	const topLabel =
-		topCategory && topCategory in TOP_CATEGORY_DATA
-			? TOP_CATEGORY_DATA[topCategory as keyof typeof TOP_CATEGORY_DATA]
-					.label
-			: undefined;
-	const subLabel =
-		subCategory && CATEGORY_DATA[subCategory]
-			? CATEGORY_DATA[subCategory].label
-			: undefined;
+}: NoResultsProps) => (
+	<div className="no-results">
+		<p className="no-results__message">No foods match your search.</p>
+		<div className="no-results__actions">
+			<CategoryClearLink
+				subCategory={subCategory}
+				topCategory={topCategory}
+			/>
 
-	return (
-		<div className="no-results">
-			<p className="no-results__message">No foods match your search.</p>
-			<div className="no-results__actions">
-				{subCategory && topCategory && (
-					<Link className="no-results__button" to={`/${topCategory}`}>
-						Show All &ldquo;{topLabel ?? topCategory}&rdquo;
-					</Link>
-				)}
+			<button
+				className="no-results__button"
+				onClick={onClearSearch}
+				type="button"
+			>
+				Clear Search
+			</button>
 
-				{!subCategory && topCategory && (
-					<Link className="no-results__button" to="/">
-						Clear Category
-					</Link>
-				)}
-
-				{subCategory && !topCategory && subLabel && (
-					<Link className="no-results__button" to="/">
-						Clear Category
-					</Link>
-				)}
-
+			{hasDifficulty && (
 				<button
 					className="no-results__button"
-					onClick={onClearSearch}
+					onClick={onClearDifficulty}
 					type="button"
 				>
-					Clear Search
+					Clear Difficulty
 				</button>
-
-				{hasDifficulty && (
-					<button
-						className="no-results__button"
-						onClick={onClearDifficulty}
-						type="button"
-					>
-						Clear Difficulty
-					</button>
-				)}
-			</div>
+			)}
 		</div>
-	);
-};
+	</div>
+);
