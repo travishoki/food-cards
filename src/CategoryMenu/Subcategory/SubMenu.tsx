8a8ab83880d1a29764b6e78448410f6a2a5640de
Travis Hoki <travis.hoki@gmail.com>
2026-05-27 12:33:03 -0600

    subcategory resetting on route change

--- src/CategoryMenu/Subcategory/SubMenu.tsx
+++ src/CategoryMenu/Subcategory/SubMenu.tsx
@@ -1,19 +1,30 @@
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 
 import { FilterSection } from "../../FilterSection/FilterSection";
+import { TOP_CATEGORIES } from "../../const/categories";
+import { LOCATIONS } from "../../const/locations.const";
 import { useFoodFilters } from "../../context/foodFilters";
 import { useUrlParams } from "../../hooks/useUrlParams";
 import { subCategoriesForTop } from "../CategoryMenu.helpers";
 
 export const SubMenu = () => {
 	const { setSubCategory, subCategory } = useFoodFilters();
-	const { urlTopCategory } = useUrlParams();
+	const { urlLocation, urlTopCategory } = useUrlParams();
 	const subMenu = useMemo(
 		() => (urlTopCategory ? subCategoriesForTop(urlTopCategory) : []),
 		[urlTopCategory],
 	);
 
+	const sweetHidden =
+		urlTopCategory === TOP_CATEGORIES.sweets &&
+		urlLocation !== LOCATIONS.home;
+
+	useEffect(() => {
+		if (sweetHidden) setSubCategory(null);
+	}, [sweetHidden, setSubCategory]);
+
 	if (subMenu.length <= 1) return null;
+	if (sweetHidden) return null;
 
 	return (
 		<FilterSection ariaLabel="Food subcategories" label="Subcategory:">
