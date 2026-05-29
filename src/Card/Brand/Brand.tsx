836a010410597a69d259ad411b2105485754025a
Travis Hoki <travis.hoki@gmail.com>
2026-05-29 16:39:16 -0600

    remove brand logo if error

--- src/Card/Brand/Brand.tsx
+++ src/Card/Brand/Brand.tsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
+
 import "./Brand.scss";
 
 type BrandProps = {
 	brand: string;
 };
 
-export const Brand = ({ brand }: BrandProps) => (
-	<img
-		alt=""
-		className="brand-img"
-		loading="lazy"
-		src={`/brands/${brand}.png`}
-	/>
-);
+export const Brand = ({ brand }: BrandProps) => {
+	const [errored, setErrored] = useState(false);
+
+	if (errored) return null;
+
+	return (
+		<img
+			alt=""
+			className="brand-img"
+			loading="lazy"
+			onError={() => setErrored(true)}
+			src={`/brands/${brand}.png`}
+		/>
+	);
+};
